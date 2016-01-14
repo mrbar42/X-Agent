29b10be3d2a0c529db753541a0656e8ef503b0ec
MrBar42 <mrbar42@users.noreply.github.com>
2016-01-14 11:05:38 +0200

    updated user agent string

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36";
+var userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36";
 
 var requestFilter = {
     urls: [
