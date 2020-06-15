853281374beef81dca74bc56c0956cc704844631
MrBar42 <mrbar42@users.noreply.github.com>
2020-06-15 11:21:57 +0300

    update user agent string

--- xagent.js
+++ xagent.js
@@ -1,5 +1,5 @@
 (function () {
-    var USERAGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36';
+    var USERAGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36';
     var VENDOR = '';
     var PLATFORM = 'Windows';
 
